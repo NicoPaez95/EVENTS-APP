b3d36bfca0d2b0ce4fe1c03c4da5e93fdcd15d0a
NicoPaez95 <nicopaezcba4@gmail.com>
2026-02-10 22:26:24 -0300

    feat(events): connect events page to router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,17 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Events from './events/pages/Events';
+
 function App() {
-  return <h1>Events App funcionando 🚀</h1>
+  return (
+    <BrowserRouter>
+      <main>
+        <Routes>
+          <Route path="/" element={<Events />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
